9fd6582005a136bd1a13bea3b525e213ef58cd98
Delovoa <dm-github2@brainthaw.me>
2026-05-02 12:19:07 -0600

    Update steam.sh

--- optional/steam.sh
+++ optional/steam.sh
@@ -2,6 +2,18 @@
 set -euo pipefail
 
 # -----------------------------
-# 1. Install Steam
+# 1. Install GE-Proton
+# -----------------------------
+yay -S proton-ge-custom-bin
+
+# -----------------------------
+# 2. Install GameMode & GameScope
+# -----------------------------
+sudo pacman -S gamemode lib32-gamemode gamescope
+sudo groupadd -r gamemode
+sudo usermod -aG video,audio,input,gamemode $USER
+
+# -----------------------------
+# 3. Install Steam
 # -----------------------------
 sudo pacman -S steam steam-devices
