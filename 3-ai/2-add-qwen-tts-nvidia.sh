e83c3d23a9fdb370cc4509d681df097e3c6f3386
Delovoa <dm-github2@askthesphinx.com>
2026-05-08 15:02:57 -0600

    Updated scripts

--- 3-ai/2-add-qwen-tts-nvidia.sh
+++ 3-ai/2-add-qwen-tts-nvidia.sh
@@ -10,7 +10,7 @@ micromamba create -n qwen3-tts python=3.12 ipykernel -y
 eval "$(micromamba shell hook --shell bash)"
 micromamba activate qwen3-tts
 
-pip install -U qwen-tts
+pip install -U qwen-tts soundfile ipywidgets
 
 python -m ipykernel install --user --name qwen-tts --display-name "QWEN-TTS"
 micromamba deactivate
